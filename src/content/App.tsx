a4c445b6350ea143fecb0c317d66829a07de8928
David <nundavid99@gmail.com>
2022-07-26 12:01:26 -0400

    Navigator on App + initial LoginPage

--- src/content/App.tsx
+++ src/content/App.tsx
@@ -1,13 +1,29 @@
 import React, { FC } from 'react';
+import { connect } from 'react-redux';
+import { LOGIN_PAGE } from './redux/nav/constants';
+import { RootState } from './redux/store';
+import LoginPage from './screens/LoginPage';
+import './styles/main.scss';
 
 interface IProps {
-
+	currentPage: string, //from redux
 };
 
-const App: FC<IProps> = () => {
-	return (
-		<div>App!!</div>
-	);
-};
+// This component is the entry point for the react app and acts as an navigator too
+// It will use the nav redux store to direct the user to the different pages
 
-export default App;
+const App: FC<IProps> = ({ currentPage }) => {
+	let cmp = <LoginPage/>
+	switch (currentPage) {
+		case LOGIN_PAGE:
+		default:
+			cmp = <LoginPage/>
+			break;
+	}
+	return cmp;
+};
+const mapStateToProps = (state: RootState) => {
+	const { nav } = state;
+	return { currentPage: nav.currentPage };
+}
+export default connect(mapStateToProps, {})(App);
---
+++ src/content/screens/LoginPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = () => {
+	return (
+		<main>
+			Login!
+		</main>
+	)
+}
+
+export default LoginPage;
