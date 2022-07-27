d3e16516c32d304a3b1a434e88dc584a0be6452a
David <nundavid99@gmail.com>
2022-07-27 10:02:30 -0400

    Nav reducer type correction

--- src/content/redux/nav/reducers.ts
+++ src/content/redux/nav/reducers.ts
@@ -1,3 +1,4 @@
+import { AnyAction } from "redux";
 import { LOGIN_PAGE } from "./constants";
 
 const INIT_STATE = {
@@ -6,7 +7,7 @@ const INIT_STATE = {
 // type ACTIONTYPE =
 // 	| { type: "increment"; payload: number }
 // 	| { type: "decrement"; payload: string };
-const NavReducer = (state = INIT_STATE, action: any) => {
+const NavReducer = (state = INIT_STATE, action: AnyAction) => {
 	switch(action.type) {
 		default:
 			return state;
