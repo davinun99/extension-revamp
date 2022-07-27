55834603799d5900bf4976eaf1181fac10081bb0
David <nundavid99@gmail.com>
2022-07-27 10:04:04 -0400

    Login action with messages and added nav consts

--- src/content/redux/auth/actions.ts
+++ src/content/redux/auth/actions.ts
@@ -1,4 +1,24 @@
+import { Action, ThunkAction, ThunkDispatch } from "@reduxjs/toolkit";
+import { LOGIN_MESSAGE } from "../../../helpers/constants";
 import { LOGIN, LOGIN_ERROR, LOGIN_SUCCESS } from "./constants";
+
+/**
+ * https://redux.js.org/usage/usage-with-typescript#type-checking-redux-thunks
+ * export type ThunkAction< 
+  R, // Return type of the thunk function
+  S, // state type used by getState
+  E, // any "extra argument" injected into the thunk
+  A extends Action // known types of actions that can be dispatched
+> = (dispatch: ThunkDispatch<S, E, A>, getState: () => S, extraArgument: E) => R
+ */
+type MyThunkResult<R> = ThunkAction<R, any, any, Action>;
+
+export const loginAction = (): MyThunkResult<Promise<void>> => {
+	return async(dispatch: ThunkDispatch<void, any, Action> ) => {
+		dispatch(login);
+		chrome.runtime.sendMessage({message: LOGIN_MESSAGE});
+	};
+};
 
 export const login = () => ({
 	type: LOGIN,
--- src/content/redux/nav/constants.ts
+++ src/content/redux/nav/constants.ts
@@ -1,1 +1,2 @@
 export const LOGIN_PAGE:string = 'LOGIN_PAGE';
+export const HOME_PAGE:string = 'HOME_PAGE';
