e750fd5254693f770aff98d8292f4edb9473edf3
David <nundavid99@gmail.com>
2022-07-27 10:02:12 -0400

    Applying thunk to the store

--- src/content/redux/store.ts
+++ src/content/redux/store.ts
@@ -1,19 +1,16 @@
 // @flow
 import { configureStore } from '@reduxjs/toolkit';
 import AuthReducer from './auth/reducers';
-// import { applyMiddleware } from 'redux';
-// import createSagaMiddleware from 'redux-saga';
-// import sagas from './sagas';
-
-// const sagaMiddleware = createSagaMiddleware();
-// const middlewares = [sagaMiddleware];
+import thunk from "redux-thunk";
 
 //REDUCERS: 
 import CandidateReducer from './candidate/reducers';
 import NavReducer from './nav/reducers';
+
 export const store = configureStore({ reducer: {
 	auth: AuthReducer,
 	candidate: CandidateReducer,
 	nav: NavReducer,
-} });
+}, middleware: [thunk] });
+
 export type RootState = ReturnType<typeof store.getState>
