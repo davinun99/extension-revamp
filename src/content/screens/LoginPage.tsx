1f6bab37dea1fc6837722cb79de0b5af22a6c6d5
David <nundavid99@gmail.com>
2022-07-28 10:11:03 -0400

    Login page v1 finished

--- src/content/screens/LoginPage.tsx
+++ src/content/screens/LoginPage.tsx
@@ -1,28 +1,33 @@
 import React, { FC } from 'react';
 import { connect } from 'react-redux';
-import { Container, Button } from 'reactstrap';
+import { Container, Button, Spinner } from 'reactstrap';
 import { RootState } from '../redux/store';
 import { loginAction } from '../redux/auth/actions';
 import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
 interface IProps {
+	isLoading: boolean, //from redux
 	loginAction: Function, //from redux
 };
 const LoginPage:FC<IProps> = (
-	{ loginAction }
+	{ isLoading, loginAction }
 ) => {
 	const handleLogin = () => {
 		loginAction();
 	}
 	return (
 		<Container className='loginContainer'>
-			<Button color='primary' className='extensionBtn' onClick={handleLogin}>
-				Login
-			</Button>
+			{
+				isLoading ? <Spinner size='sm'/> : 
+				<Button color='primary' className='extensionBtn' onClick={handleLogin}>
+					Login
+				</Button>
+			}
 		</Container>
 	)
 }
 const mapStateToProps = (state: RootState) => {
-	return {  };
+	const { isLoading } = state.auth;
+	return { isLoading };
 }
 const mapDispatchToProps = (dispatch :ThunkDispatch<any, any, AnyAction>) => ({
 	loginAction: () => dispatch(loginAction())
