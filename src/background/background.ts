4ea38a565375ff368782a7bc068ef4dbd8de237d
David <nundavid99@gmail.com>
2022-07-28 10:08:43 -0400

    background fixed to send message and store userAuth info

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,10 +1,10 @@
 import { LOGIN_MESSAGE, BACKEND_URL, GOOGLE_CLIENT_ID } from "../helpers/constants"
 
-const login = () => new Promise((resolve, reject) => {
+const login = (): Promise<any> => new Promise((resolve, reject) => {
 	let responseURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${encodeURIComponent(GOOGLE_CLIENT_ID)}&scope=openid%20email%20profile&response_type=code&redirect_uri=${encodeURIComponent(`${BACKEND_URL}/auth/google/callback/v2`)}&resource=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fauth`;
 	chrome.identity.launchWebAuthFlow( {url: responseURL, interactive: true}, (redirectUrl: string | undefined) => {
 		if (chrome.runtime.lastError) {
-			console.log('Error in auth flow', chrome.runtime.lastError);
+			//Error in auth flow', chrome.runtime.lastError
 			resolve(null);
 			return;
 		} else if (redirectUrl){
@@ -13,12 +13,12 @@ const login = () => new Promise((resolve, reject) => {
 				userObj = JSON.parse(decodeURIComponent(userObj));
 				resolve(userObj);
 			} catch (error) {
-				console.log('Error parsing response to get user', error);
+				// Error parsing response to get user, error
 				resolve(null);
 				return;
 			}
 		} else {
-			console.log('Error en auth, redirectUrl: ', redirectUrl);
+			// Error en auth, redirectUrl is null
 			resolve(null);
 			return;
 		}
@@ -26,14 +26,13 @@ const login = () => new Promise((resolve, reject) => {
 });
 chrome.runtime.onMessage.addListener(async (request: BackgroundMessage, sender, sendResponse ) => {
 	if (request.message === LOGIN_MESSAGE) { //Front is trying to init login
-		const userAuth = await login();
-		chrome.tabs.query({}, (tabs) => { //Responding to all tabs
-			const message = { message: LOGIN_MESSAGE, payload: userAuth };
-			tabs.forEach(tab => {
-				if (typeof tab.id === 'number') {
-					chrome.tabs.sendMessage(tab.id, message);
-				}
-			});
-		});
+		const userAuth:Object = await login();
+		const message = { message: LOGIN_MESSAGE, payload: userAuth };
+		if(sender.tab?.id ){
+			chrome.tabs.sendMessage(sender.tab.id, message);
+		}
+		if(userAuth){
+			chrome.storage.local.set({auth: userAuth});
+		}
 	}
 })
