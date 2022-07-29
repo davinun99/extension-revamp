263f1c2b00f3c13a4a94296a4a0dbd7b4067100f
David <nundavid99@gmail.com>
2022-07-29 01:25:46 -0400

    Home initial view

--- src/content/screens/HomePage.tsx
+++ src/content/screens/HomePage.tsx
@@ -1,21 +1,30 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { GET_AUTH_MESSAGE } from '../../helpers/constants';
 import { RootState } from '../redux/store';
-import { AnyAction, ThunkDispatch } from '@reduxjs/toolkit';
 
 interface IProps {
-	
+	authData: any, //from redux
 };
-const HomePage:FC<IProps> = (
-) => {
+const HomePage:FC<IProps> = ({
+	authData
+}) => {
 	return (
-		<main className='container-fluid HomeContainer'>
-			Home!
+		<main className='container HomeContainer mt-3'>
+			<h3 className='mt-3'>Welcome {authData?.google_user?.name}!</h3>
+			<section className='mt-2'>
+				<h5>Last visited profiles:</h5>
+				<ul>
+					<li>Jose's</li>
+					<li>Manuel's</li>
+				</ul>
+			</section>
 		</main>
 	)
-}
+};
 const mapStateToProps = (state: RootState) => {
-	return {  };
+	const { auth } = state;
+	return { authData: auth.authData };
 }
-const mapDispatchToProps = (dispatch :ThunkDispatch<any, any, AnyAction>) => ({});
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+// const mapDispatchToProps = (dispatch :ThunkDispatch<any, any, AnyAction>) => ({});
+export default connect(mapStateToProps, {})(HomePage);
