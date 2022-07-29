24865d7eb85392299527a57ff5e61416ca293418
David <nundavid99@gmail.com>
2022-07-29 01:25:37 -0400

    AuthData type

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -4,3 +4,30 @@ type BackgroundMessage = {
 	message: string,
 	payload: Object,
 }
+type AuthData = {
+	nimbl_user: {
+		user_id: number,
+		first_name: string,
+		last_name: string,
+		email: string,
+		user_type_id: number
+	},
+	google_user: {
+		"sub": string,
+		"name": string,
+		"given_name": string,
+		"family_name": string,
+		"picture": string,
+		"email": string,
+		"email_verified": boolean,
+		"locale": string,
+		"hd": string
+	},
+	tokens: {
+		access_token: string,
+		expires_at: EpochTimeStamp,
+		scope: string,
+		token_type: string,
+		id_token: string
+	}
+};
