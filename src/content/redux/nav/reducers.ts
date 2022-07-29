03a960f0a5fe6d3fd008d8c412eafbc15efd9b2a
David <nundavid99@gmail.com>
2022-07-29 01:25:27 -0400

    Fixed nav reducer

--- src/content/redux/nav/reducers.ts
+++ src/content/redux/nav/reducers.ts
@@ -1,5 +1,5 @@
 import { AnyAction } from "redux";
-import { LOGIN_PAGE } from "./constants";
+import { HOME_PAGE, LOGIN_PAGE } from "./constants";
 
 const INIT_STATE = {
 	currentPage: LOGIN_PAGE,
@@ -7,8 +7,16 @@ const INIT_STATE = {
 // type ACTIONTYPE =
 // 	| { type: "increment"; payload: number }
 // 	| { type: "decrement"; payload: string };
-const NavReducer = (state = INIT_STATE, action: AnyAction) => ({
-	...state,
-	currentPage: action.type,
-})
+const NavReducer = (state = INIT_STATE, action: AnyAction) => {
+	switch(action.type) {
+		case HOME_PAGE:
+		case LOGIN_PAGE:
+			return {
+				...state,
+				currentPage: action.type,
+			};
+		default: 
+			return state;
+	}
+}
 export default NavReducer;
