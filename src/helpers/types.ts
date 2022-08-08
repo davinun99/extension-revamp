0c6b136a5e05fd90a3b388c752d8e1b3f171bc55
David <nundavid99@gmail.com>
2022-08-08 11:58:06 -0400

    Types corrected

--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -16,15 +16,15 @@ type AuthData = {
 		user_type_id: number
 	},
 	google_user: {
-		"sub": string,
-		"name": string,
-		"given_name": string,
-		"family_name": string,
-		"picture": string,
-		"email": string,
-		"email_verified": boolean,
-		"locale": string,
-		"hd": string
+		sub: string,
+		name: string,
+		given_name: string,
+		family_name: string,
+		picture: string,
+		email: string,
+		email_verified: boolean,
+		locale: string,
+		hd: string
 	},
 	tokens: {
 		access_token: string,
