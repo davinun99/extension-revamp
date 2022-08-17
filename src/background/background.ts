0f9bdd783b8a565045fb9b949d55896ae0e492be
David <nundavid99@gmail.com>
2022-08-17 10:40:49 -0400

    Background helpers separated on new module

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,62 +1,6 @@
-import { LOGIN_MESSAGE, GET_AUTH_MESSAGE, BACKEND_URL, GOOGLE_CLIENT_ID, URL_CHANGE_MESSAGE } from "../helpers/constants"
+import { LOGIN_MESSAGE, GET_AUTH_MESSAGE, URL_CHANGE_MESSAGE } from "../helpers/constants"
+import { login, getAuthData, sendLoginInfoToExistingTabs } from './helpers';
 
-/**
- * It opens a new tab with a Google login page, and when the user logs in, it gets the user's data and
- * closes the tab
- * @returns AuthData.
- * @returns null.
- */
-const login = (): Promise<AuthData| null> => new Promise((resolve, reject) => {
-	let responseURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${encodeURIComponent(GOOGLE_CLIENT_ID)}&scope=openid%20email%20profile&response_type=code&redirect_uri=${encodeURIComponent(`${BACKEND_URL}/auth/google/callback/v2`)}&resource=https%3A%2F%2Faccounts.google.com%2Fo%2Foauth2%2Fauth`;
-	chrome.identity.launchWebAuthFlow( {url: responseURL, interactive: true}, (redirectUrl: string | undefined) => {
-		if (chrome.runtime.lastError) {
-			//Error in auth flow', chrome.runtime.lastError
-			resolve(null);
-			return;
-		} else if (redirectUrl){
-			let userObjStr:string = redirectUrl.substring( redirectUrl.indexOf(`?result=`) + 8 );
-			try {
-				const userObj:AuthData = JSON.parse(decodeURIComponent(userObjStr));
-				resolve(userObj);
-			} catch (error) {
-				// Error parsing response to get user, error
-				resolve(null);
-				return;
-			}
-		} else {
-			// Error en auth, redirectUrl is null
-			resolve(null);
-			return;
-		}
-	});
-});
-
-/**
- * It gets the auth data from the sync storage and returns a promise that resolves to either an AuthData object or null
- * @returns AuthData.
- * @returns null.
- */
-const getAuthData = async ():Promise<AuthData|null> => {
-	const authData = await chrome.storage.sync.get(['auth']);
-	if (authData?.auth) {
-		return authData.auth;
-	} else {
-		return null;
-	}
-};
-
-/**
- * It sends a message to all open LinkedIn tabs
- * @param {BackgroundMessage} message - BackgroundMessage - The message to send to the content script.
- */
-const sendLoginInfoToExistingTabs = async(message: BackgroundMessage) => {
-	const tabs = await chrome.tabs.query({url: 'https://www.linkedin.com/*'});
-	tabs.forEach(tab => {
-		try{
-			tab.id && chrome.tabs.sendMessage(tab.id, message);
-		}catch(e){ }
-	});
-}
 chrome.runtime.onMessage.addListener(async (request: BackgroundMessageJustType, sender, sendResponse ) => {
 	if (!sender.tab?.id){ //If there's not tabId, we don't need to process this msg
 		return;
@@ -79,7 +23,7 @@ chrome.runtime.onMessage.addListener(async (request: BackgroundMessageJustType, 
 			payload = authData;
 		} else {
 			//Handle token expiration
-			error = { message: 'Error getting your user information, please try logging in again.' };
+			// error = { message: 'Error getting your user information, please try logging in again.' };
 		}
 		const message: BackgroundMessage = { message: GET_AUTH_MESSAGE, payload, error };
 		sendLoginInfoToExistingTabs(message);
@@ -101,14 +45,3 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 		chrome.tabs.sendMessage(tabId, message);
 	}
 });
-/**
- * It returns the last 10 LinkedIn profiles you've visited
- * @returns An array of objects.
- */
-const getLastVisitedCandidates = async () => {
-	const result = await chrome.history.search({
-		maxResults: 10,
-		text: 'https://www.linkedin.com/in/'
-	});
-	return result;
-}
