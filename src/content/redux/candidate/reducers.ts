c650cafd56e7e9dff1dc5c01afab5277f81024e6
David <nundavid99@gmail.com>
2022-08-22 07:57:39 -0400

    setting scraped candidate info on candidate reducer

--- src/content/redux/candidate/reducers.ts
+++ src/content/redux/candidate/reducers.ts
@@ -1,9 +1,19 @@
-const INIT_STATE = {};
+import { GET_CANDIDATE_SCRAPED } from "./constants";
+
+const INIT_STATE = {
+	scrapedCandidateInfo: null,
+	backenCandidateInfo: null,
+};
 // type ACTIONTYPE =
 // 	| { type: "increment"; payload: number }
 // 	| { type: "decrement"; payload: string };
 const CandidateReducer = (state = INIT_STATE, action: any) => {
 	switch(action.type) {
+		case GET_CANDIDATE_SCRAPED:
+			return {
+				...state,
+				scrapedCandidateInfo: action.payload,
+			};
 		default:
 			return state;
 	}
