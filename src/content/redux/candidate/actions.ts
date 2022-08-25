cc0ce38601be977030cee92aa499e7543bb99a49
David <nundavid99@gmail.com>
2022-08-25 10:46:28 -0400

    get candidatte notes

--- src/content/redux/candidate/actions.ts
+++ src/content/redux/candidate/actions.ts
@@ -2,7 +2,7 @@ import { ThunkDispatch } from '@reduxjs/toolkit';
 import { Action } from "redux";
 import { ThunkAction } from "redux-thunk";
 import { getCandidateFromScrape } from '../../../helpers/scrape';
-import { GET_CANDIDATE_FROM_BACK, GET_CANDIDATE_SCRAPED, GET_CANDIDATE_FROM_BACK_SUCCESS, GET_CANDIDATE_FROM_BACK_ERROR, GET_CANDIDATE_MESSAGES, GET_CANDIDATE_MESSAGES_SUCCESS, SAVE_CANDIDATE } from "./constants";
+import { GET_CANDIDATE_FROM_BACK, GET_CANDIDATE_SCRAPED, GET_CANDIDATE_FROM_BACK_SUCCESS, GET_CANDIDATE_FROM_BACK_ERROR, SAVE_CANDIDATE, GET_CANDIDATE_NOTES, GET_CANDIDATE_NOTES_SUCCESS, GET_CANDIDATE_NOTES_ERROR } from "./constants";
 import * as BackEnd from '../../../helpers/https';
 
 type CandidateThunkResult<R> = ThunkAction<R, undefined, undefined, Action>;
@@ -24,6 +24,17 @@ export const getCandidateFromBackAction = (url: string): CandidateThunkResult<vo
 			dispatch(getCandidateFromBackSuccess(candidate));
 		}else {
 			dispatch(getCandidateFromBackError('There was an error getting the candidate'));
+		}	
+	};
+};
+export const getCandidateNotesAction = (candidateId:number): CandidateThunkResult<void> => {
+	return async (dispatch: ThunkDispatch<void, any, Action> ) => {
+		dispatch(getCandidateNotes());
+		const notes:BackendCandidateNote[]|null = await BackEnd.getCandidateNotes(candidateId);
+		if(notes){
+			dispatch(getCandidateNotesSuccess(notes));
+		}else {
+			dispatch(getCandidateNotesError('There was an error getting candidate notes'));
 		}	
 	};
 };
@@ -51,15 +62,15 @@ export const getCandidateFromBackError = (errorMessage: string) => ({
 	type: GET_CANDIDATE_FROM_BACK_ERROR,
 	payload: errorMessage,
 });
-export const getCandidateMessages = () => ({
-	type: GET_CANDIDATE_MESSAGES,
+export const getCandidateNotes = () => ({
+	type: GET_CANDIDATE_NOTES,
 });
-export const getCandidateMessagesSuccess = (messages: any) => ({
-	type: GET_CANDIDATE_MESSAGES_SUCCESS,
-	payload: messages,
+export const getCandidateNotesSuccess = (notes: any[]) => ({
+	type: GET_CANDIDATE_NOTES_SUCCESS,
+	payload: notes,
 });
-export const getCandidateMessagesError = (errorMessage:string) => ({
-	type: GET_CANDIDATE_MESSAGES_SUCCESS,
+export const getCandidateNotesError = (errorMessage:string) => ({
+	type: GET_CANDIDATE_NOTES_ERROR,
 	payload: errorMessage,
 });
 export const saveCandidate = () => ({
